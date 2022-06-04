e199e312c9b042ca4234092ecbcfa603af5e7ba5
chendingya <211250094@smail.nju.edu.cn>
2022-06-04 17:48:37 +0800

    修改过后的筛选topk，先前版本筛选方法无法正常得到排序后的对应行数，现在可以得到，但是无法做到相似值相同的行数排序后并列

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ if __name__ == "__main__":
     sheet = wb2["关联关系"]
     max_row = sheet.max_row
     max_col = sheet.max_column
-    print(max_row, " ", max_col)
 
     # creat Array:
     board = ['s' for i in range(max_row)]
-    similarities = [0 for i in range(max_row)]
+    similarities_of_data_element = [0 for i in range(max_row)]
+
+    # read the data element:
     count = 0
     for row in sheet.rows:
         board[count] = row[3].value
@@ -30,25 +31,40 @@ if __name__ == "__main__":
     command = (list(input("Please enter k and the string you want to compare").split()))
     k = command[0]  # the first is topk
     lens = len(command)
+    print("lens =", lens)
+
     # only have : topk \ B \ D
     if lens == 3:
         for i in range(0, max_row):
-            similarities[i] = difflib.SequenceMatcher(None, command[2], board[i]).ratio()
+            similarities_of_data_element[i] = difflib.SequenceMatcher(None, command[2], board[i]).ratio()
     # only have : topk \ A \ B \ D
-    if lens == 4:
+    elif lens == 4:
         for i in range(0, max_row):
-            similarities[i] = difflib.SequenceMatcher(None, command[3], board[i]).ratio()
-
+            similarities_of_data_element[i] = difflib.SequenceMatcher(None, command[3], board[i]).ratio()
 
     # choose the top10k:
     num_of_chosen = 10 * int(k)
     if num_of_chosen > max_row:
         num_of_chosen = max_row
-    largestTopk = heapq.nlargest(num_of_chosen, similarities)
+    num_dict = {}
+    for i in range(len(similarities_of_data_element)):
+        num_dict[i] = similarities_of_data_element[i]
+    res_list = sorted(num_dict.items(), key=lambda e: e[1])
+    largestTopk_index = list([one[0] for one in res_list[::-1][:num_of_chosen]])
+
+    ###
 
     for i in range(0, num_of_chosen):
-        print(largestTopk[i])
+        print(largestTopk_index[i], end=' ')
+        print(similarities_of_data_element[largestTopk_index[i]])
+    ###
 
+    # compare the largest similarities' topk:
+    similarities_of_institute_name = ['s' for i in range(0, num_of_chosen)]
+    similarities_of_table_Chinese_name = ['s' for i in range(0, num_of_chosen)]
 
-
-
+    # read the data element:
+    count = 0
+    for row in sheet.rows:
+        board[count] = row[3].value
+        count += 1
